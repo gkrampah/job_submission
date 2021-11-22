53164cc789bfa0cb5fbcdbcaab66472bf3fb698c
gkrampah <65491585+gkrampah@users.noreply.github.com>
2021-11-22 22:09:07 +0100

    Add files via upload
    script

---
+++ py_submit.py
@@ -0,0 +1,30 @@
+import os
+import numpy as np
+
+def mkdir_p(dir):
+    '''make a directory (dir) if it doesn't exist'''
+    if not os.path.exists(dir):
+        os.mkdir(dir)
+
+job_directory = f"{os.getcwd()}/data"
+
+#this creates a directory if that dir doesn't already exist
+mkdir_p(job_directory)
+
+energies = 10**np.linspace(19, 25, 16)
+
+names = np.linspace(1, 16, 16, dtype=int)
+
+for ene, name in zip(energies, names):
+
+    job_file = os.path.join(job_directory, f'submit_{name}.job')
+
+    with open(f'{job_file}', 'w') as fh:
+        #write whatever you need in your script here
+        fh.writelines("#!/bin/bash\n")
+        
+        fh.writelines("source /user/anaconda3/bin/activate\n")
+
+        fh.writelines(f"~/anaconda3/bin/python /user/prop_sim_final_high.py {ene} {name}\n" )     
+
+    #os.system(f"qsub -lnodes=1: {job_file}")
